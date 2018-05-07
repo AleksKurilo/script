c9baf4a730d42476006a40e5c925cdddbf70c0a2
Alexandr Kurilo <akurilo@apextech.com.ua>
2018-05-07 13:28:15 +0300

    add .gitignore

--- src/Binding.java
+++ src/Binding.java
@@ -1,9 +1,9 @@
 public class Binding {
 
     public final static String FILE_NAME = "result.csv";
-    public final static String PATH_NEW_DIR = "/Users/aleksk/Desktop/GitHub/scripts/statistics/data/new_model/";
-    public final static String PATH_OLD_DIR = "/Users/aleksk/Desktop/GitHub/scripts/statistics/data/old_model/";
+//    public final static String PATH_NEW_DIR = "/Users/aleksk/Desktop/GitHub/scripts/statistics/data/new_model/";
+//    public final static String PATH_OLD_DIR = "/Users/aleksk/Desktop/GitHub/scripts/statistics/data/old_model/";
 
-//    public final static String PATH_NEW_DIR = "C:/Users/Aleks/Desktop/new/";
-//    public final static String PATH_OLD_DIR = "C:/Users/Aleks/Desktop/old/";
+    public final static String PATH_NEW_DIR = "C:/Users/Aleks/Desktop/new/";
+    public final static String PATH_OLD_DIR = "C:/Users/Aleks/Desktop/old/";
 }
